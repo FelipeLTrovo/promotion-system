506a6ff8f34124a4c0b99d67fa1800c694a2eed8
Felipe Leal Trovo <felipeltrovo@hotmail.com>
2021-03-28 22:08:56 -0300

    Adds the ability to enable a coupon that has been desactivated previously.

--- test/system/coupons_test.rb
+++ test/system/coupons_test.rb
@@ -14,5 +14,19 @@ class CouponsTest < ApplicationSystemTestCase
         assert_text "#{coupon.code} (desabilitado)"
         assert_no_text 'Desabilitar'
     end
+
+    test 'enable a coupon' do
+        promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                code: 'NATAL10', discount_rate: 10, coupon_quantity: 3,
+                expiration_date: '22/12/2033')
+        coupon = Coupon.create!(code: 'BLABLABLA', status: :disabled, promotion: promotion)
+        login_user
+        visit promotion_path(promotion)
+        click_on 'Habilitar'
+
+        assert_text "Cupom #{coupon.code} habilitado com sucesso"
+        assert_text "#{coupon.code} (ativo)"
+        assert_no_text 'Habilitar'
+    end
     
 end
